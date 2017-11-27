2f54eac0d266abefd1afad097887f368eb5ae428
atlie <atlie17@ru.is>
2017-11-27 22:42:40 +0000

    verk 1 a test

--- tilraun/main.cpp
+++ tilraun/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "this is a call" << endl;
+    string str;
+    ifstream fin;
+    int ten_lines = 9;
+    bool continue_running;
+    char continue_char;
+    int i = 0;
+
+    fin.open("lorem.txt");
+    if(fin.is_open()){
+        do{
+            for(; i <= ten_lines;i++)
+                {
+                getline(fin, str);
+                cout << str << endl;
+                }
+        fin.close();
+
+    cout << "Do you want to continue (y/n)?";
+    cin >> continue_char;
+    if(continue_char == 'y'){
+        continue_running = true;
+        i = 0;
+    }
+    else{
+        continue_running = false;
+    }
+        }
+        while(continue_running == true);
+
+
     return 0;
+}
 }
